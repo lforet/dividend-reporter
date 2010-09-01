f8ea3e965d9bffdbd0693370f0be34af3e98f778
Adam Dill <altrux@gmail.com>
2010-09-01 10:16:36 -0500

    [RAILS] generated rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DividendReporter::Application.config.secret_token = '02d884926a3fe0e5a58121f0cdafb8b865b978ec83042ca968f591726c0b34291380212ac170a8197da37052614776ee27228c149407b274f6469e03712d03bd'
