d06f8714573a7248bd7ef43354aeb69845828d74
Adam Dill <altrux@gmail.com>
2010-09-08 15:35:59 -0500

    [STOCK] added basic stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ActiveRecord::Base
+
+  has_many :quotes
+
+  def to_s
+    "#{ticker} - #{name}"
+  end
+
+  scope :active, where(:active => true)
+  scope :inactive, where(:active => false)
+
 end
