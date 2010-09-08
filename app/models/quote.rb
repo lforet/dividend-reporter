9dfde559c9c8686fa9c9605e9374e3e213b2bc09
Adam Dill <altrux@gmail.com>
2010-09-08 15:36:28 -0500

    [QUOTE] added basic quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,17 @@
+require File.join(Rails.root, "lib", "yahoo_integrator.rb")
+
 class Quote < ActiveRecord::Base
+
+  belongs_to :stock
+
+  def to_s
+    ""
+  end
+
+
+  def self.gather_quotes(stocks)
+    yi = YahooIntegrator.new(@quotes)
+    yi.run
+  end
+  
 end
